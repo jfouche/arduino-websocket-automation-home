7ef5ac4c5cea723f0fdaf7513456519a2d837c7c
jfouche <jeremie.fouche@gmail.com>
2016-09-26 11:11:18 +0200

    simplified the application

--- application/dashboard.ts
+++ application/dashboard.ts
@@ -2,24 +2,8 @@ import { ConnectionController } from './components/connection.ts'
 import { LoggerController } from './components/logger.ts'
 import { TemperatureController } from './components/temperature.ts'
 
-let c: DashboadController;
-
-/**
- * DashboadController
- */
-class DashboadController  {
-
-    connectionController: ConnectionController
-    loggerController: LoggerController;
-    temperatureController: TemperatureController;
-
-    constructor() {
-        this.connectionController = new ConnectionController();
-        this.loggerController = new LoggerController();
-        this.temperatureController = new TemperatureController();
-    }
-}
-
 $(function () {
-    c = new DashboadController();
+    new ConnectionController();
+    new TemperatureController();
+    new LoggerController();
 });
